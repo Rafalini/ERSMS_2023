9064b423025811ef3dffaf96a22c0cbb987522bb
Marcin Piotrowski <mpio@netcompany.com>
2023-05-27 13:15:19 +0200

    cloud dependecies, git ignores, clean up

---
+++ webapp/src/main/java/com/ersms/app/GCPStorage.java
@@ -0,0 +1,12 @@
+package com.ersms.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/gcp")
+public class GCPStorage {
+//    @Autowired
+//    private Storage storage;
+}
