f48515838d54fe2c8371799194cbfadbeb9317ec
Marcin Piotrowski <mpio@netcompany.com>
2023-05-27 21:32:21 +0200

    test wiadra

--- webapp/src/main/java/com/ersms/app/GCPStorage.java
+++ webapp/src/main/java/com/ersms/app/GCPStorage.java
@@ -1,12 +1,30 @@
 package com.ersms.app;
 
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.BlobInfo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("/gcp")
 public class GCPStorage {
-//    @Autowired
-//    private Storage storage;
+    @Autowired
+    private Storage storage;
+    @GetMapping("/send-data")
+    public String sendData() throws IOException {
+        BlobId id = BlobId.of("wiadro-na-dane", "santa.jpg");
+        BlobInfo info = BlobInfo.newBuilder(id).build();
+        File file = new File("santa.jpg");
+        byte[] arr = Files.readAllBytes(Paths.get(file.toURI()));
+        storage.create(info, arr);
+        return "Success!";
+    }
 }
