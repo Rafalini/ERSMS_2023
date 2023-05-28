b8f36d61dddb73a43e9ac0711dc1a9da986a9dfc
Aleksander Grzybowski <olek.grzybowski@gmail.com>
2023-05-28 18:48:13 +0200

    moved unused microservices to example folder

--- webapp/src/main/java/com/ersms/app/Webapp.java
+++ webapp/src/main/java/com/ersms/app/Webapp.java
@@ -2,12 +2,10 @@ package com.ersms.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Description;
 
 @SpringBootApplication
-public class Main {
+public class Webapp {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(Webapp.class, args);
     }
 }
