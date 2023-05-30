c11cbe57ae4d55a9edc8d532b332ca3f437f262f
jszymane <j.szymanek@stud.elka.pw.edu.pl>
2023-05-30 15:45:18 +0200

    add function to retrieve image labels

--- image-analyzer/src/main/java/com.ersms.app/service/ImageService.java
+++ image-analyzer/src/main/java/com.ersms.app/service/ImageService.java
@@ -9,6 +9,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -33,8 +35,13 @@ public class ImageService {
             imageRepository.save(imageEntity);
         }
 
-        // Perform image analysis using Google Cloud Vision API
-        List<String> imageTags = extractTagsFromImage(imageUrl);
+        List<String> imageTags = new ArrayList<>();
+
+        try {
+            imageTags = extractTagsFromImage(imageUrl);
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
 
         for (String tag : imageTags) {
             ImageTagEntity imageTagEntity = imageTagRepository.findByName(tag);
@@ -50,12 +57,38 @@ public class ImageService {
         }
     }
 
-    private List<String> extractTagsFromImage(String imageUrl) {
-        // Code to use Google Cloud Vision API and extract image labels/tags
-        // Replace with your own implementation using the Google Cloud Vision API client library
+    private List<String> extractTagsFromImage(String imageUrl) throws IOException {
 
-        // For this example, let's assume we have a list of hardcoded tags
-        return List.of("tag1", "tag2", "tag3");
+        List<String> tags = new ArrayList<>();
+
+        // Create an Image instance from the URL
+        ImageSource imageSource = ImageSource.newBuilder().setImageUri(imageUrl).build();
+        Image image = Image.newBuilder().setSource(imageSource).build();
+
+        // Create a feature request for label detection
+        Feature feature = Feature.newBuilder().setType(Feature.Type.LABEL_DETECTION).build();
+        AnnotateImageRequest request = AnnotateImageRequest.newBuilder().addFeatures(feature).setImage(image).build();
+
+        // Authenticate and create an ImageAnnotatorClient
+        try (ImageAnnotatorClient client = ImageAnnotatorClient.create()) {
+            // Send the request and receive the response
+            BatchAnnotateImagesResponse response = client.batchAnnotateImages(List.of(request));
+
+            // Process the response
+            for (AnnotateImageResponse annotateResponse : response.getResponsesList()) {
+                if (annotateResponse.hasError()) {
+                    System.err.println("Error: " + annotateResponse.getError().getMessage());
+                    return tags;
+                }
+
+                // Extract and add the tags to the list
+                for (EntityAnnotation annotation : annotateResponse.getLabelAnnotationsList()) {
+                    tags.add(annotation.getDescription());
+                }
+            }
+        }
+
+        return tags;
     }
 }
 
